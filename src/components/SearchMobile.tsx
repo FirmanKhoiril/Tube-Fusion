d2d0eb05a3ae16c2186622bb9e67aee1805d53f1
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-08-02 16:39:57 +0700

    searchmobile functionality done

--- src/components/SearchMobile.tsx
+++ src/components/SearchMobile.tsx
@@ -1,35 +1,77 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { IoIosSearch } from "react-icons/io";
-import {motion} from 'framer-motion'
+import {AnimatePresence, motion} from 'framer-motion'
+import { IoClose,IoArrowBackOutline  } from "react-icons/io5";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 const SearchMobile = () => {
     const [expandSearchBarMobile, setExpandSearchBarMobile] = useState(false)
+    const [showClearButtonSearchMobile, setShowClearButtonSearchMobile] = useState(false);
+    const [inputSearch, setInputSearch] = useSearchParams({
+        q: ""
+    })
+    const query = inputSearch.get("q")  || ""
+    const navigate = useNavigate()
+    useEffect(() => {
+      if(query.length !== 0) {
+        setShowClearButtonSearchMobile(true)
+      }
+      return () => {
+        setShowClearButtonSearchMobile(false)
+      }
+    }, [query])
+
+    const handleSubmitSearching = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        navigate(`/${query}`)
+    }
 
   return (
     <>
-    <form className={`sm:hidden w-full`}>
-         <button type="button" onClick={( ) => setExpandSearchBarMobile(true)} className="p-2 bg-gray-100/5 shadow-sm hover:bg-gray-100/10 transition duration-150 rounded-full">
+         <button type="button" onClick={( ) => setExpandSearchBarMobile(true)} className="sm:hidden inline-block p-2 bg-gray-100/5 shadow-sm hover:bg-gray-100/10 transition duration-150 rounded-full">
             <IoIosSearch size={24} className="icon" />
         </button>
-        
-    </form>
-
-    {expandSearchBarMobile && (
-                <motion.div
-                    initial={{ scaleX: 0 }}
-                    animate={{ scaleX: 1 }}
-                    transition={{ duration: 0.3 }}
-                    className="fixed z-50 left-2 top-0 transform -translate-x-1/2 mt-3 w-[96.6%] px-4 py-2 rounded-full flex  bg-dark-0 origin-center"
+    <AnimatePresence>
+        {expandSearchBarMobile && (
+                <motion.form
+                onSubmit={handleSubmitSearching}
+                    initial={{ scaleY: 0, scaleX: 0 }}
+                    animate={{ scaleY: 1, scaleX: 1 }}
+                    exit={{ scaleY: 0, scaleX: 0 }}
+                    transition={{ duration: 0.25 }}
+                    className="fixed z-50 flex sm:hidden items-center justify-between left-2 top-0 transform -translate-x-1/2 mt-3 w-[96.6%]  rounded-full   bg-dark-0 origin-center"
                 >
+                    
+                    <button type="button" onClick={( ) => setExpandSearchBarMobile(false)} className=" py-3.5 pl-4 pr-2 icon">
+                        <IoArrowBackOutline size={22} className="" />
+                    </button>
                     <input
                         type="text"
-                        className="w-full bg-dark-0 pr-4 pl-7 text-gray-200 outline-none text-[14px] p-2 rounded-full"
+                        value={query}
+                        onChange={(e) => setInputSearch(prev => {
+                            prev.set("q", e.target.value)
+                            return prev
+                        }, {
+                            replace: true
+                        })}
+                        className="w-full bg-dark-0 pr-[52px] pl-3 placeholder:text-gray-400 focus:placeholder:text-gray-200 text-gray-200 outline-none text-[14px] py-2 rounded-full"
                         placeholder="Search..."
                     />
-                </motion.div>
+                    <button type="submit" className=" py-3.5 pl-2 pr-4 icon">
+                        <IoIosSearch size={24} />
+                    </button>
+                    {query.length !== 0 && (
+                    <button type="button" className={`${showClearButtonSearchMobile ? "opacity-100" : " opacity-0"} transition ease-in-out duration-200`}>
+                        <IoClose size={24} onClick={() => setInputSearch((prev) => {
+                            prev.set("q", "")
+                            return prev
+                        })} className="absolute top-[14px] right-14 icon" />
+                    </button>
+                    )}
+                </motion.form>
             )}
-
+    </AnimatePresence>
     </>
     
   )
