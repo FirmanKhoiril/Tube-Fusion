f200ee89bbc0598b4711f23b00bef09e99326e29
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-08-02 19:22:13 +0700

    fixing env

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 const queryClient = new QueryClient()
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_test_Y2F1c2FsLW1vbml0b3ItMzMuY2xlcmsuYWNjb3VudHMuZGV2JA" || import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
