3b51fbb27bd32bb2a8e3aaab84157a63934292cd
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-08-16 16:26:44 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,36 +9,3 @@ export default defineConfig({
     port: 3000
   }
 })
-
-//,  VitePWA({
-//   registerType: 'autoUpdate',
-//   workbox: {
-//     clientsClaim: true,
-//     skipWaiting: true
-//   },
-//   devOptions: {
-//     enabled: true
-//   },
-//   manifest: {
-//     name: 'Fusioner',
-//     short_name: 'Fusioner',
-//     description: 'Your Ultimate Cryptocurrency Companion',
-//     theme_color: '#ffffff',
-//     icons: [
-//       {
-//         src: '/maskable-icon.png',
-//         sizes: '192x192',
-//         type: 'image/png',
-//         purpose: 'any'
-//       },
-//       {
-//         src: '/maskable-icon.png',
-//         sizes: '512x512',
-//         type: 'image/png'
-//       }
-//     ]
-//   },
-//   injectRegister: null, // Disable the default injection
-//   srcDir: 'src', // Assuming serviceWorker.js is in the src directory
-//   filename: '/serviceWorker.js', // Change the filename
-// })
